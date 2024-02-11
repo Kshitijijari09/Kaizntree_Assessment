a9d952a1674bfa98c81fc5a526f91db0c20580c2
Kshitijijari09 <ijarikshitij9@gmail.com>
2024-02-11 10:34:24 -0800

    Completed Frontend Tasks

---
+++ src/components/InputCheckbox/index.tsx
@@ -0,0 +1,24 @@
+import classNames from "classnames"
+import { useRef } from "react"
+import { InputCheckboxComponent } from "./types"
+
+export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
+  const { current: inputId } = useRef(`KaizntreeInputCheckbox-${id}`)
+
+  return (
+    <div className="KaizntreeInputCheckbox--container" data-testid={inputId}>
+      <input
+       className={classNames("KaizntreeInputCheckbox--label", {
+        "KaizntreeInputCheckbox--label-checked": checked,
+        "KaizntreeInputCheckbox--label-disabled": disabled,
+      })}
+        id={inputId}
+        type="checkbox"
+        // className="KaizntreeInputCheckbox--input"
+        checked={checked}
+        disabled={disabled}
+        onChange={() => onChange(!checked)}
+      />
+    </div>
+  )
+}
